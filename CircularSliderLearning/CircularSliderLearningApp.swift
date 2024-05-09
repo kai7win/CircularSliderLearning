12057d33ab7e6030a7f5587779607d25fdfe383f
KaiChi <34007164+kai7win@users.noreply.github.com>
2024-05-09 20:02:15 +0800

    Initial Commit

---
+++ CircularSliderLearning/CircularSliderLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSliderLearningApp.swift
+//  CircularSliderLearning
+//
+//  Created by Kai Chi Tsao on 2024/5/9.
+//
+
+import SwiftUI
+
+@main
+struct CircularSliderLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularSliderLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircularSliderLearning
+//
+//  Created by Kai Chi Tsao on 2024/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
